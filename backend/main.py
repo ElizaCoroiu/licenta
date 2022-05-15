bd21694ab72ec5e084502f2b62b660d13cf8afde
Eliza Coroiu <ecoroiu31@gmail.com>
2022-05-15 22:19:44 +0300

    modified main file from backend: post method and CORS added

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from image_processing import process_image
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 all_symbols = process_image.get_notes('resources/images/mary.jpg')
 
-@app.get("/api/GetNotes")
+@app.get("/notes")
 async def get():
     return all_symbols
 
+@app.post("/file")
+async def create_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
